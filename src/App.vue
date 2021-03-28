a14b0a5495dffb86b2668c800cca40bba7d51ac3
Zywy <piotr@zywolewski.eu>
2021-03-28 18:54:10 +0200

    Created Vue chat using firebase app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang='pug'>
+h1 Hello
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
